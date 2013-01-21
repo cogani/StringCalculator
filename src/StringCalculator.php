fbd90e4b630fd96a80d1611b405d16638be87e48
charly <cogani@yahoo.es>
2013-01-21 18:00:01 +0100

    Test suma 0 parametros (Caso 1.2c)  - Dos parametros

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -11,9 +11,24 @@
  * @author nanopc
  */
 class StringCalculator {
-    public function add($string){
-        return 0;
+
+    public function add($string) {
+        $result = 0;
+
+        if ($string == NULL || $string == "")
+            return $result;
+
+        $token = strtok($string, ',');
+        if ($token)
+            $result = $token;
+
+        $token = strtok(',');
+        if ($token)
+            $result = $result + $token;
+
+        return $result;
     }
+
 }
 
 ?>
--- test/src/StringCalculatorTest.php
+++ test/src/StringCalculatorTest.php
@@ -11,7 +11,12 @@ class StringCalculatorTest extends PHPUnit_Framework_TestCase {
 
     public function testAdd1Parameters() {
         $stCalculator = new StringCalculator();
-        $this->assertEquals(1, $stCalculator->add("2"));
+        $this->assertEquals(2, $stCalculator->add("2"));
+    }
+
+    public function testAdd2Parameters() {
+        $stCalculator = new StringCalculator();
+        $this->assertEquals(23, $stCalculator->add("12.5,10.5"));
     }
 
 }
