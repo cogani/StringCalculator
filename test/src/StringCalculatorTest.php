71c5c921cfde39eca9ec86a7b7a07576b72a08a4
charly <cogani@yahoo.es>
2013-01-21 17:29:28 +0100

    Test suma 0 parametros (Caso 1.2b)  - Un solo parametro

--- test/src/StringCalculatorTest.php
+++ test/src/StringCalculatorTest.php
@@ -1,13 +1,17 @@
 <?php
+
 require_once '../../src/StringCalculator.php';
 
 class StringCalculatorTest extends PHPUnit_Framework_TestCase {
 
-   
-
-    public function testAdd() {
+    public function testAdd0Parameters() {
         $stCalculator = new StringCalculator();
-          $this->assertEquals(0, $stCalculator->add(""));
+        $this->assertEquals(0, $stCalculator->add(""));
+    }
+
+    public function testAdd1Parameters() {
+        $stCalculator = new StringCalculator();
+        $this->assertEquals(1, $stCalculator->add("2"));
     }
 
 }
