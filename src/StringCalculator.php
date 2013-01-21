cda6bd571a838f23519b632565a28ae46dce9d12
user.mail <cogani@yahoo.es>
2013-01-21 14:59:14 +0100

    Test suma 0 parametros (Caso 1.2a)

---
+++ src/StringCalculator.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of StringCalculator
+ *
+ * @author nanopc
+ */
+class StringCalculator {
+    public function add($string){
+        return 0;
+    }
+}
+
+?>
---
+++ test/src/StringCalculatorTest.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../../src/StringCalculator.php';
+
+class StringCalculatorTest extends PHPUnit_Framework_TestCase {
+
+   
+
+    public function testAdd() {
+        $this->markTestIncomplete(
+                'This test has not been implemented yet.'
+        );
+    }
+
+}
